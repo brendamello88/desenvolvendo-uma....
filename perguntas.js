92226f4269aa105f1031126fdfcd78464c6ec643
b.mellogv <brenda.vornes.mello@escola.pr.gov.br>
2025-08-08 19:47:25 -0300

    cartas de crepuscolo

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Vampiros',
+    'Qual o nome do clã de vampiros de Forks?',
+    'O clã Cullen.'
+)
+
+criaCartao(
+    'Lobisomens',
+    'Qual o nome completo do líder da matilha?',
+    'Jacob Black.'
+)
+
+criaCartao(
+    'Personagens',
+    'Qual o dom de Alice Cullen?',
+    'Ela consegue ver o futuro.'
+)
+
+criaCartao(
+    'Vampiros',
+    'Qual a cor dos olhos dos vampiros "vegetarianos"?',
+    'Os olhos são dourados.'
+)
+
+criaCartao(
+    'Lugares',
+    'Onde os Volturi moram?',
+    'Eles moram em Volterra, na Itália.'
+)
