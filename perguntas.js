5adc1912bcafd5cd699bc6f081a5195f00a96cea
b.mellogv <brenda.vornes.mello@escola.pr.gov.br>
2025-08-08 20:01:14 -0300

    finalizado

--- perguntas.js
+++ perguntas.js
@@ -27,3 +27,38 @@ criaCartao(
     'Onde os Volturi moram?',
     'Eles moram em Volterra, na Itália.'
 )
+criaCartao(
+    'Vampiros',
+    'Qual o nome do clã de vampiros de Forks?',
+    'O clã Cullen.'
+)
+
+criaCartao(
+    'Lobisomens',
+    'Qual o nome completo do líder da matilha?',
+    'Jacob Black.'
+)
+
+criaCartao(
+    'Personagens',
+    'Qual o dom de Alice Cullen?',
+    'Ela consegue ver o futuro.'
+)
+
+criaCartao(
+    'Vampiros',
+    'Qual a cor dos olhos dos vampiros "vegetarianos"?',
+    'Os olhos são dourados.'
+)
+
+criaCartao(
+    'Lugares',
+    'Onde os Volturi moram?',
+    'Eles moram em Volterra, na Itália.'
+)
+
+criaCartao(
+    'Vampiros',
+    'Qual a dieta do clã Cullen?',
+    'Eles se alimentam de sangue de animais.'
+)
